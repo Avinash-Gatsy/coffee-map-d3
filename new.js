ae16f2d58517af29421ba5614378af4527100110
Avinash <avinashg12291992@gmail.com>
2018-10-04 16:12:40 +0530

    initial commit

---
+++ new.js
@@ -0,0 +1,56 @@
+[
+	{
+		"name" :  "Acquire",
+		"children" : [
+			{
+				"name" : "Application Pre-fill",
+				"children" : [
+					{
+					"name" : "Tech Warriors"
+					}
+				]
+			},
+			
+			{
+				"name" : "Apply for Credit",
+				"children" : [
+					{
+					"name" : "Star Lords"
+					}
+				]
+			},
+			
+			{
+				"name" : "Registration",
+				"children" : [
+					{
+						"name" : "Transforms"
+					},
+					{
+						"name" : "Agni"
+					}
+				]
+			}
+		]
+	},
+	
+	{
+		"name" :  "Enable",
+		"children" : [
+			{
+				"name" : "Foundation",
+				"children" : [
+					{
+						"name" : "Star Tech"
+					},
+					{
+						"name" : "Deceptions"
+					},
+					{
+						"name" : "Himavat"
+					}
+				]
+			}
+		]
+	}
+]
---
+++ old.js
@@ -0,0 +1,254 @@
+[
+  {
+    "name": "Acquire",
+    "children": [
+      {
+        "name": "Application Pre-fill",
+        "children": [
+          {
+            "name": "Tech Warriors"
+          }
+        ]
+      },
+      {
+        "name": "Apply for Credit",
+        "children": [
+          {
+            "name": "Star Lords"
+          }
+        ]
+      },
+      {
+        "name": "Registration",
+        "children": [
+          {
+            "name": "Transformers"
+          },
+          {
+            "name": "Agni"
+          }
+        ]
+      }
+    ]
+  },
+  
+  
+  {
+    "name": "Enable",
+    "children": [
+      {
+        "name": "Foundation",
+        "children": [
+          {
+            "name": "Star Tech"
+          },
+          {
+            "name": "Deceptions"
+          },
+          {
+            "name": "Himavat"
+          }
+        ]
+      }
+    ]
+  },
+  
+  
+  {
+    "name": "Grow",
+    "children": [
+      {
+        "name": "SOM Marketing",
+        "children": [
+          {
+            "name": "Deceptions"
+          }
+        ]
+      },
+      {
+        "name": "Manage MyRewards",
+        "children": [
+          {
+            "name": "Samudra"
+          }
+        ]
+      },
+      {
+        "name": "Promo Balance",
+        "children": [
+          {
+            "name": "Surya"
+          }
+        ]
+      }
+    ]
+  },
+  
+  
+  {
+    "name": "Identify",
+    "children": [
+      {
+        "name": "Confirm Identity",
+        "children": [
+          {
+            "name": "Quality Kings"
+          },
+          {
+            "name": "Transformers"
+          },
+          {
+            "name": "Team X"
+          }
+        ]
+      }
+    ]
+  },
+  
+  
+  {
+    "name": "Pay",
+    "children": [
+      {
+        "name": "Money Movement",
+        "children": [
+          {
+            "name": "Nagas"
+          },
+          {
+            "name": "Himavat"
+          }
+        ]
+      }
+    ]
+  },
+  
+  
+  {
+    "name": "Service",
+    "children": [
+      {
+        "name": "Account Financials",
+        "children": [
+          {
+            "name": "Deceptions"
+          },
+          {
+            "name": "Varuna"
+          }
+        ]
+      },
+      {
+        "name": "Account Relationships",
+        "children": [
+          {
+            "name": "Avengers"
+          },
+          {
+            "name": "Agni"
+          }
+        ]
+      },
+      {
+        "name": "Alerts/ Notifications",
+        "children": [
+          {
+            "name": "Simbas"
+          },
+          {
+            "name": "Varuna"
+          }
+        ]
+      },
+      {
+        "name": "Credit LimitIncrease",
+        "children": [
+          {
+            "name": "CD Demigods"
+          }
+        ]
+      },
+      {
+        "name": "Customer Management",
+        "children": [
+          {
+            "name": "Avengers"
+          },
+          {
+            "name": "Team X"
+          }
+        ]
+      },
+      {
+        "name": "Document Management",
+        "children": [
+          {
+            "name": "Simbas"
+          },
+          {
+            "name": "INC Slayers"
+          }
+        ]
+      },
+      {
+        "name": "Initiate Disputes",
+        "children": [
+          {
+            "name": "Nagas"
+          }
+        ]
+      },
+      {
+        "name": "Post ApprovalSetup",
+        "children": [
+          {
+            "name": "House Stark"
+          }
+        ]
+      },
+      {
+        "name": "Reports/ Analytics",
+        "children": [
+          {
+            "name": "Garudas"
+          }
+        ]
+      },
+      {
+        "name": "Secure Messaging",
+        "children": [
+          {
+            "name": "Simbas"
+          }
+        ]
+      },
+      {
+        "name": "Transactions",
+        "children": [
+          {
+            "name": "Jedi"
+          },
+		  {
+            "name": "INC Slayers"
+          }
+        ]
+      },
+      {
+        "name": "Resolve",
+        "children": [
+          {
+            "name": "Simbas"
+          }
+        ]
+      },
+      {
+        "name": "Report Lost/Stolen",
+        "children": [
+          {
+            "name": "Simbas"
+          }
+        ]
+      }
+    ]
+  }
+]
